a95a81f15b917490aa8a1ab3082d3a2a9972f382
mcproger <mcproger@localhost.localdomain>
2017-10-16 15:58:21 +0300

    Replace menu manage function

--- js/sidebar_menu.js
+++ js/sidebar_menu.js
@@ -1,11 +1,4 @@
-// Closes the sidebar menu
-$("#menu-close").click(function(e) {
+$("#sidebar-wrapper a").click(function (e) {
   e.preventDefault();
-  $("#sidebar-wrapper").toggleClass("active");
-});
-
-// Opens the sidebar menu
-$("#menu-toggle").click(function(e) {
-  e.preventDefault();
-  $("#sidebar-wrapper").toggleClass("active");
+  $("#sidebar-wrapper").removeClass("active");
 });
